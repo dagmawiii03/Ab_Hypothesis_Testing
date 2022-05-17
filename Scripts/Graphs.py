41688b9b59cae56e6d694dca0e5d307a265ceab1
dagi <dagmawi.yoha03@gmail.com>
2022-05-17 10:41:49 +0300

    adding funciton which does heatmap

--- Scripts/Graphs.py
+++ Scripts/Graphs.py
@@ -57,3 +57,12 @@ class draw:
         plt.title(f'Distribution of User {column}', size=20, fontweight='bold')
         plt.xlabel('Number of Users')
         plt.show()
+
+    def plot_heatmap(df: pd.DataFrame, title: str, cbar=False) -> None:
+        
+        plt.figure(figsize=(12, 7))
+        sns.heatmap(df, annot=True, cmap='viridis', vmin=0,
+                    vmax=1, fmt='.2f', linewidths=.7, cbar=cbar)
+        plt.title(title, size=18, fontweight='bold')
+        plt.show()
+
