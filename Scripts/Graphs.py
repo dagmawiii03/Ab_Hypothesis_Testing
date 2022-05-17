c555c4b8a12d3a0223ebdf9b87ffdd672e97a51b
dagi <dagmawi.yoha03@gmail.com>
2022-05-17 10:23:06 +0300

    defining function to box plot in graph file

--- Scripts/Graphs.py
+++ Scripts/Graphs.py
@@ -5,8 +5,9 @@ import pandas as pd
 import seaborn as sns
 
 def plot_bar(df: pd.DataFrame, x_col: str, y_col: str, title: str, xlabel: str, ylabel: str) -> None:
-    #defined function to do par plot with the following structure
-
+    
+    '''defined function to do bar plot with the following structure
+'''
     plt.figure(figsize=(12, 7))
     sns.barplot(data=df, x=x_col, y=y_col)
     plt.title(title, size=20)
@@ -15,4 +16,13 @@ def plot_bar(df: pd.DataFrame, x_col: str, y_col: str, title: str, xlabel: str, 
     plt.xlabel(xlabel, fontsize=16)
     plt.ylabel(ylabel, fontsize=16)
     plt.show()
+
+def plot_box(df: pd.DataFrame, x_col: str, title: str) -> None:
     
+    '''defined function to do box plot with the following structure
+'''
+    plt.figure(figsize=(12, 7))
+    sns.boxplot(data=df, x=x_col)
+    plt.title(title, size=20)
+    plt.xticks(rotation=75, fontsize=14)
+    plt.show()
