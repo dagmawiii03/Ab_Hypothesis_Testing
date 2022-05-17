027b2152a71fcfba7daf2a48c6500b7ecf7a3841
dagi <dagmawi.yoha03@gmail.com>
2022-05-17 10:46:46 +0300

    does simple scatter plot

--- Scripts/Graphs.py
+++ Scripts/Graphs.py
@@ -75,11 +75,18 @@ class draw:
         plt.show()
     
     def plot_box_multi(df: pd.DataFrame, x_col: str, y_col: str, title: str) -> None:
-        
+
         plt.figure(figsize=(12, 7))
         sns.boxplot(data=df, x=x_col, y=y_col)
         plt.title(title, size=20)
         plt.xticks(rotation=75, fontsize=14)
         plt.yticks(fontsize=14)
         plt.show()
-
+    
+    def simple_plot_scatter(df: pd.DataFrame, x_col: str, y_col: str, title: str) -> None:
+    plt.figure(figsize=(12, 7))
+    sns.scatterplot(data=df, x=x_col, y=y_col)
+    plt.title(title, size=20)
+    plt.xticks(fontsize=14)
+    plt.yticks(fontsize=14)
+    plt.show()
