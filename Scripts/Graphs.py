3e0377b6a79385449463c86077cc8f50634883f4
dagi <dagmawi.yoha03@gmail.com>
2022-05-17 10:25:36 +0300

    defining function to scatter plot in graph file

--- Scripts/Graphs.py
+++ Scripts/Graphs.py
@@ -26,3 +26,14 @@ def plot_box(df: pd.DataFrame, x_col: str, title: str) -> None:
     plt.title(title, size=20)
     plt.xticks(rotation=75, fontsize=14)
     plt.show()
+
+def plot_scatter(df: pd.DataFrame, x_col: str, y_col: str, title: str, hue: str, style: str) -> None:
+     
+    #defined function to do box plot with the following structure
+
+    plt.figure(figsize=(12, 7))
+    sns.scatterplot(data=df, x=x_col, y=y_col, hue=hue, style=style)
+    plt.title(title, size=20)
+    plt.xticks(fontsize=14)
+    plt.yticks(fontsize=14)
+    plt.show()
