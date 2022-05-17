e05f25b3e3c6a025efc2235e565ea97248711234
dagi <dagmawi.yoha03@gmail.com>
2022-05-17 10:37:57 +0300

    adding function which does histograms

--- Scripts/Graphs.py
+++ Scripts/Graphs.py
@@ -4,36 +4,47 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
 
-def plot_bar(df: pd.DataFrame, x_col: str, y_col: str, title: str, xlabel: str, ylabel: str) -> None:
-    
-    '''defined function to do bar plot with the following structure
-'''
-    plt.figure(figsize=(12, 7))
-    sns.barplot(data=df, x=x_col, y=y_col)
-    plt.title(title, size=20)
-    plt.xticks(rotation=75, fontsize=14)
-    plt.yticks(fontsize=14)
-    plt.xlabel(xlabel, fontsize=16)
-    plt.ylabel(ylabel, fontsize=16)
-    plt.show()
+class draw:
 
-def plot_box(df: pd.DataFrame, x_col: str, title: str) -> None:
-    
-    '''defined function to do box plot with the following structure
-'''
-    plt.figure(figsize=(12, 7))
-    sns.boxplot(data=df, x=x_col)
-    plt.title(title, size=20)
-    plt.xticks(rotation=75, fontsize=14)
-    plt.show()
+    def plot_bar(df: pd.DataFrame, x_col: str, y_col: str, title: str, xlabel: str, ylabel: str) -> None:
 
-def plot_scatter(df: pd.DataFrame, x_col: str, y_col: str, title: str, hue: str, style: str) -> None:
+    
+        '''defined function to do bar plot with the following structure
+    '''
+        plt.figure(figsize=(12, 7))
+        sns.barplot(data=df, x=x_col, y=y_col)
+        plt.title(title, size=20)
+        plt.xticks(rotation=75, fontsize=14)
+        plt.yticks(fontsize=14)
+        plt.xlabel(xlabel, fontsize=16)
+        plt.ylabel(ylabel, fontsize=16)
+        plt.show()
+
+    def plot_box(df: pd.DataFrame, x_col: str, title: str) -> None:
+    
+        '''defined function to do box plot with the following structure
+    '''
+        plt.figure(figsize=(12, 7))
+        sns.boxplot(data=df, x=x_col)
+        plt.title(title, size=20)
+        plt.xticks(rotation=75, fontsize=14)
+        plt.show()
+
+    def plot_scatter(df: pd.DataFrame, x_col: str, y_col: str, title: str, hue: str, style: str) -> None:
      
-    #defined function to do box plot with the following structure
+        #defined function to do box plot with the following structure
 
-    plt.figure(figsize=(12, 7))
-    sns.scatterplot(data=df, x=x_col, y=y_col, hue=hue, style=style)
-    plt.title(title, size=20)
-    plt.xticks(fontsize=14)
-    plt.yticks(fontsize=14)
-    plt.show()
+        plt.figure(figsize=(12, 7))
+        sns.scatterplot(data=df, x=x_col, y=y_col, hue=hue, style=style)
+        plt.title(title, size=20)
+        plt.xticks(fontsize=14)
+        plt.yticks(fontsize=14)
+        plt.show()
+
+    def plot_hist(df: pd.DataFrame, column: str, color: str) -> None:
+        # plt.figure(figsize=(15, 10))
+        # fig, ax = plt.subplots(1, figsize=(12, 7))
+        sns.displot(data=df, x=column, color=color, kde=True, height=7, aspect=2)
+        plt.title(f'Distribution of user {column}', size=20, fontweight='bold')
+        plt.show()
+
