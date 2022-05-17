a1d9e7e1500e36372d523181e55c37aa5646d22a
dagi <dagmawi.yoha03@gmail.com>
2022-05-17 10:18:32 +0300

    defining function to bar plot in graph file

--- Scripts/Graphs.py
+++ Scripts/Graphs.py
@@ -3,3 +3,16 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
+
+def plot_bar(df: pd.DataFrame, x_col: str, y_col: str, title: str, xlabel: str, ylabel: str) -> None:
+    #defined function to do par plot with the following structure
+
+    plt.figure(figsize=(12, 7))
+    sns.barplot(data=df, x=x_col, y=y_col)
+    plt.title(title, size=20)
+    plt.xticks(rotation=75, fontsize=14)
+    plt.yticks(fontsize=14)
+    plt.xlabel(xlabel, fontsize=16)
+    plt.ylabel(ylabel, fontsize=16)
+    plt.show()
+    
