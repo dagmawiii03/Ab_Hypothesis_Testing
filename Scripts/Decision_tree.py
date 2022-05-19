06ca812f0df7a96fe3e60a2792753beb5dc3b507
dagi <dagmawi.yoha03@gmail.com>
2022-05-19 15:02:23 +0300

    add train funciton

--- Scripts/Decision_tree.py
+++ Scripts/Decision_tree.py
@@ -33,5 +33,53 @@ class handler:
   def  get_app_logger(self) -> logging.Logger:
     return self.logger
 
+class DecisionTreesModel:
+    
+    def __init__(self, X_train, X_test, y_train, y_test, max_depth=5):
+        
+        self.X_train = X_train
+        self.X_test = X_test
+        self.y_train = y_train
+        self.y_test = y_test
+        
+        self.clf = DecisionTreeClassifier(max_depth=4)
+        self.logger = handler("models.log").get_app_logger()
+
+        
+    def train(self, folds=1):
+        
+        kf = KFold(n_splits = folds)
+        
+        iterator = kf.split(self.X_train)
+        
+        loss_arr = []
+        acc_arr = []
+        self.logger.info(f"Model DecisionTreesModel training started")
+
+        for i in range(folds):
+            train_index, valid_index = next(iterator)
+            
+            X_train, y_train = self.X_train.iloc[train_index], self.y_train.iloc[train_index]
+            X_valid, y_valid = self.X_train.iloc[valid_index], self.y_train.iloc[valid_index]
+                        
+            self.clf = self.clf.fit(X_train, y_train)
+            
+            vali_pred = self.clf.predict(X_valid)
+            
+            accuracy = self.calculate_score(y_valid
+                                              , vali_pred)
+            
+            loss = loss_function(y_valid, vali_pred)
+            
+            self.__printAccuracy(accuracy, i, label="Validation")
+            self.__printLoss(loss, i, label="Validation")
+            print()
+            
+            acc_arr.append(accuracy)
+            loss_arr.append(loss)
+
+            
+        return self.clf, acc_arr, loss_arr
+
 
 
