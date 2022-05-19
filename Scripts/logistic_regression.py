0b05e8ff69f8892d6e7420041428ad6e1b5c5100
dagi <dagmawi.yoha03@gmail.com>
2022-05-19 15:31:34 +0300

    add important modules

---
+++ Scripts/logistic_regression.py
@@ -0,0 +1,10 @@
+import seaborn as sns
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from sklearn.linear_model import LogisticRegression
+from sklearn import metrics
+from sklearn.model_selection import KFold
+import scipy.stats as stat
+from sklearn.metrics import mean_squared_error
+
